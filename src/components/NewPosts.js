1b4fc69bacf40d140228359dcf7bc5cece7eb654
Soren Axelson <sorenaxelson@gmail.com>
2023-03-16 16:44:14 -0500

    scuffed

--- src/components/NewPosts.js
+++ src/components/NewPosts.js
@@ -1,5 +1,74 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/2211-FTB-ET-WEB-FT`;
 const NewPosts = () => {
-  return <div>ewrgiuhb</div>;
+  const navigate = useNavigate();
+  const [title, enterTitle] = useState("");
+  const [price, enterPrice] = useState("");
+  const [description, enterDescription] = useState("");
+  const [delivery, enterDelivery] = useState("");
+  let deliveryBoolean = false;
+
+  const makePost = async () => {
+    if (delivery === "y" || delivery === "Y") {
+      deliveryBoolean = true;
+    } else {
+      deliveryBoolean = false;
+    }
+    try {
+      const response = await fetch(`${BASE_URL}/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${TOKEN_STRING_HERE}`,
+        },
+        body: JSON.stringify({
+          post: {
+            title: title,
+            description: description,
+            price: `$${price}`,
+            willDeliver: deliveryBoolean,
+          },
+        }),
+      });
+      const result = await response.json();
+      console.log(result);
+      navigate("./#");
+      return result;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={makePost}>
+        <label>Enter Listing Title</label>
+        <input
+          value={title}
+          type="text"
+          onChange={(event) => enterTitle(event.target.value)}
+        ></input>
+        <label>Enter Price in USD</label>
+        <input
+          value={price}
+          type="text"
+          onChange={(event) => enterPrice(event.target.value)}
+        ></input>
+        <label>Enter Item Description</label>
+        <input
+          value={description}
+          type="text"
+          onChange={(event) => enterDescription(event.target.value)}
+        ></input>
+        <label>Will You Deliver? Enter "y" or "n"</label>
+        <input
+          value={delivery}
+          type="text"
+          onChange={(event) => enterDelivery(event.target.value)}
+        ></input>
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
 };
 export default NewPosts;
